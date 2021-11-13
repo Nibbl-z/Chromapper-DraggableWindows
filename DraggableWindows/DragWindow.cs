9578fd8e1b51c0e76b39551f50de0c99cad55471
Nibbl-z <93560873+Nibbl-z@users.noreply.github.com>
2021-11-13 06:53:44 -0700

    Add files via upload

---
+++ DraggableWindows/DragWindow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragWindow : MonoBehaviour, IDragHandler
+{
+
+    public RectTransform dragRectTransform;
+    public GameObject canvas;
+    public CanvasScaler canvasScaler;
+    public float scaleFactor;
+
+    
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        canvasScaler = canvas.GetComponent<CanvasScaler>();
+        scaleFactor = Screen.width / canvasScaler.referenceResolution.x; // It's not quite perfect but it's the best I could do
+        dragRectTransform = gameObject.GetComponent<RectTransform>();
+        
+        dragRectTransform.anchoredPosition += eventData.delta / scaleFactor;
+    }
+}
--- DraggableWindows/Plugin.cs
+++ DraggableWindows/Plugin.cs
@@ -1,1 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Sprites;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+namespace DraggableWindows
+{
+    [Plugin("Draggable Windows")]
+    public class Plugin
+    {
+
+        [Init]
+        [Obsolete]
+        private void Init()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;         
+        }
+
+        [Obsolete]
+        private void SceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.buildIndex == 3)
+            {
+                GameObject mapEditorUI = GameObject.Find("MapEditorUI");
+
+                List<Transform> children = new List<Transform>(mapEditorUI.GetComponentsInChildren<Transform>());
+                foreach (var child in children)
+                {
+                    if (child.GetComponent<CanvasScaler>() != null)
+                    {
+                        switch (child.name)
+                        {
+                            case "Node Editor Canvas":
+                                AddDraggable(child.Find("Node Editor").gameObject, child.gameObject);
+                                break;
+                            case "Strobe Generator Canvas":
+                                AddDraggable(child.Find("Strobe Generator").gameObject, child.gameObject);
+                                break;
+                            case "Chroma Colour Selector":
+                                AddDraggable(child.Find("Chroma Colour Selector").Find("Picker 2.0").gameObject, child.gameObject);
+                                break;
+                            case "BPM Tapper Canvas":
+                                AddDraggable(child.Find("BPM Tapper").Find("Background Panel").gameObject, child.gameObject);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void AddDraggable(GameObject ui, GameObject parentUi)
+        {
+            Debug.Log("creating draggable");
+            ui.AddComponent<DragWindow>();
+            ui.GetComponent<DragWindow>().canvas = parentUi;
+        }
+    }
+}
